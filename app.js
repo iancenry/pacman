d56ec787218e599921a13e40f8a1fd09d5c9bc27
Ian Cenry <iancenry@gmail.com>
2023-03-24 22:29:56 +0300

    add pacman movement

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const grid = document.querySelector('.grid'),
-      scoreDisplay = document.querySelector('#score'),
-      width = 28 // 28*28 = 784 squares 
+  scoreDisplay = document.querySelector('#score'),
+  width = 28 // 28*28 = 784 squares
+let score = 0;
 
-// a grid with 784 squares   
+// a grid with 784 squares
 const layout = [
   1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
   1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
@@ -40,20 +41,22 @@ const layout = [
 //3 - power-pellet
 //4 - empty
 
-const squares = [];
+const squares = []
 
 //draw the grid and render
-function createBoard(){
+function createBoard() {
   for (let i = 0; i < layout.length; i++) {
-    const square = document.createElement('div');
+    const square = document.createElement('div')
     grid.appendChild(square)
-    squares.push(square)   
-     //add styling to the divs on the board
-     if(layout[i] === 0){
+    squares.push(square)
+    //add styling to the divs on the board
+    if (layout[i] === 0) {
       squares[i].classList.add('pac-dot')
-    }else if(layout[i] === 1) {
+    } else if (layout[i] === 1) {
       squares[i].classList.add('wall')
-    }else if(layout[i] === 3) {
+    }else if(layout[i] === 2){
+      squares[i].classList.add('ghost-lair')
+    } else if (layout[i] === 3) {
       squares[i].classList.add('power-pellet')
     }
   }
@@ -64,5 +67,43 @@ createBoard()
 //starting position of pac-man
 let pacmanCurrentIndex = 490
 squares[pacmanCurrentIndex].classList.add('pac-man')
+
+//move pac-man with key codes
+function movePacman(e) {
+  //remove pacman from square you are currently in
+  squares[pacmanCurrentIndex].classList.remove('pac-man')
+
+  switch (e.key) {
+    case 'ArrowLeft':
+      //check if pacman is in a square where the number is divisible by  the width and doesn't leave a reminder of 0
+      if (pacmanCurrentIndex % width !== 0 && !squares[pacmanCurrentIndex - 1].classList.contains('wall') && !squares[pacmanCurrentIndex - 1].classList.contains('ghost-lair')) pacmanCurrentIndex -= 1
+      //check if pacman is in the left exit
+      if((pacmanCurrentIndex - 1) === 363) pacmanCurrentIndex = 391
+      break
+    //if pacman is currently in a square where the index num if you take away 28(width) and it's > 0 you're allowed to keep moving up(one whole width down the array)
+    case 'ArrowUp':
+      if (pacmanCurrentIndex - width >= 0 && !squares[pacmanCurrentIndex - width].classList.contains('wall') && !squares[pacmanCurrentIndex - width].classList.contains('ghost-lair')) pacmanCurrentIndex -= width
+      break
+    case 'ArrowRight':
+      if (pacmanCurrentIndex % width < width - 1 && !squares[pacmanCurrentIndex + 1].classList.contains('wall') && !squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair')) pacmanCurrentIndex += 1
+      //check if pacman is in the right exit
+      if((pacmanCurrentIndex + 1) === 392) pacmanCurrentIndex = 364
+      break
+    case 'ArrowDown':
+      if (pacmanCurrentIndex + width < width * width && !squares[pacmanCurrentIndex + width].classList.contains('wall') && !squares[pacmanCurrentIndex + width].classList.contains('ghost-lair')) pacmanCurrentIndex += width
+      break
+  }
+
+  squares[pacmanCurrentIndex].classList.add('pac-man')
+
+  //pacDotEaten()
+  //powerPelletEaten()
+  //checkForWin()
+}
+
+document.addEventListener('keyup', movePacman)
+
+
+
 
 
