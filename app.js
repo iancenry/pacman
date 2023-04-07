6c5cac58bc79f77cb1d2c527e5423cec646f5d41
Ian Cenry <iancenry@gmail.com>
2023-04-08 00:04:19 +0300

    update TODO

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //TODO what are these for: gameOverId, checkWinId, leftId, rightId, upId, downId
-//TODO add pause button
+//TODO add functionality to pause button
 const grid = document.querySelector('.grid'),
   scoreDisplay = document.querySelector('#score'),
   startButton = document.getElementById('start-button'),
